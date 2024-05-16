507c780efa6479551a6dcaf271821c226f537d72
YoonDakyung <165138022+YoonDakyung@users.noreply.github.com>
2024-05-16 15:44:10 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+        if (price >= 500000 && price > 300000)
+            answer = (int)(price - (price * 0.2));
+        else if (price >= 300000 && price > 100000)
+            answer = (int)(price - (price * 0.1));
+        else if (price >= 100000)
+            answer = (int)(price - (price * 0.05));
+        else
+            answer = price;
+        return answer;
+    }
+}
