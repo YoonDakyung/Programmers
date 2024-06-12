6f603e29f3fbfb8c4b7c7a453261c13321a25638
윤다경 <165138022+YoonDakyung@users.noreply.github.com>
2024-06-12 09:18:59 +0900

    Create 학급에 따른 학과 출력하기.java

---
+++ 프로그래머스/학급에 따른 학과 출력하기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SwitchExam5 {
+         public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+    String b = "";
+    switch(a) {
+      case 1 : case 2 : case 3 : case 4 :
+          b = "뉴미디어 소프트웨어과";
+              break;
+      case 5 : case 6 :
+          b = "디자인과";
+              break;
+        }
+           System.out.println(a + "반은" + b + "입니다.");
+        }
+}
