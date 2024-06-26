56c7ef8e3bc412f03695121406758d78a3155e8f
윤다경 <165138022+YoonDakyung@users.noreply.github.com>
2024-06-26 14:54:56 +0900

    Create switch문으로 두 수 연산하기.java

---
+++ switch문으로 두 수 연산하기.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SwitchOp {
+        public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	System.out.print("수식 입력(수와 연산자 사이는 공백을 둘 것) : ");
+		int a = sc.nextInt();
+		String op = sc.next();
+		int b = sc.nextInt();
+		String answer = " ";
+
+	switch( op ) {
+		case "+" :
+			answer = Integer.toString(a + b); break;
+		case "-" :
+			answer = Integer.toString(a - b); break;
+		case "*" :
+			answer = Integer.toString(a * b); break;
+		case "/" :
+			answer = Integer.toString(a / b); break;
+		default : answer =  "사칙연산자가 아닙니다.";
+		}
+	System.out.println(a + " " + op + " " + b + " " + "=" + " " + answer);
+	}
+}
