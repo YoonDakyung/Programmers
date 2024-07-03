3056d7dd6405b90e4d27c74758a02e052923c4ee
윤다경 <165138022+YoonDakyung@users.noreply.github.com>
2024-07-03 09:03:53 +0900

    Update switch문으로 두 수 연산하기.java

--- switch문으로 두 수 연산하기.java
+++ switch문으로 두 수 연산하기.java
@@ -4,9 +4,9 @@ public class SwitchOp {
         public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
 	System.out.print("수식 입력(수와 연산자 사이는 공백을 둘 것) : ");
-		int a = sc.nextInt();
-		String op = sc.next();
-		int b = sc.nextInt();
+		int a = sc.nextInt(); //숫자
+		String op = sc.next(); //사칙연산
+		int b = sc.nextInt(); //숫자
 		String answer = " ";
 
 	switch( op ) {
