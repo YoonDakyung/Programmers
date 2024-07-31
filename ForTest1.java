c3157f2d98910cf83aa800f1ba26ccfb5b78f2bf
윤다경 <165138022+YoonDakyung@users.noreply.github.com>
2024-07-31 13:06:22 +0900

    Create ForTest1.java

---
+++ ForTest1.java
@@ -0,0 +1,14 @@
+public class ForTest1 {
+	public static void main(String args[]) {
+		int add = 0; //정수형 변수 add를 설정하고 0으로 초기화
+		
+		
+		//1부터 10까지의 수를 더하는 for문
+		for(int i = 0; i <= 10; i++) { //i를 0부터 시작해서 10까지 반복하는 for문(i에 10이 포함되어 있어야 함)
+			add += i; //현재 i의 값을 add에 누적하여 더함
+		}
+		
+		//i부터 10까지의 합을 출력
+		System.out.print("1부터 10까지의 합은 : " + add);
+	}
+}
